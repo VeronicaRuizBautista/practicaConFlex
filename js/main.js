e1a1c2eca5402cf9cd30f78adfd90c2d2630aabb
VeronicaRUizBautista <156429590+VeronicaRuizBautista@users.noreply.github.com>
2024-02-26 17:12:39 -0500

    feat: :construction: Trabajando en el Js
    Se esta acomodando la estructura par aque el menu desplegable en acordio funcione correctamente

--- js/main.js
+++ js/main.js
@@ -8,13 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
     temasItems.forEach(item => {
         item.addEventListener("click", function() {
             const subItem = this.querySelector(".ite");
+            const icon = this.querySelector(".fi.fi-sr-angle-circle-right");
             const allSubItem = document.querySelectorAll(".ite");
             allSubItem.forEach(item => {
-                if (item !== subItem && item.classList.contains("active")) {
+                if (item !== subItem && 
+                    item.classList.contains("active")) {
                     item.classList.remove("active");
                 }
             });
             subItem.classList.toggle("active");
+            if (subItem.classList.contains("active")) {
+                icon.classList.add("fi-sr-angle-circle-down");
+                icon.classList.remove("fi-sr-angle-circle-right");
+                
+            }
+            else {
+                icon.classList.add("fi-sr-angle-circle-right");
+                icon.classList.remove("fi-sr-angle-circle-down");
+            }
         });
     });
 });
